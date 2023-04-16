bd20b1d88c241fad9fe727f3acfd14f605041227
Majid Mushtaq <120600148+Majid3556@users.noreply.github.com>
2023-04-16 20:58:19 +0530

    Update streamlit_app.py
    Corrected again adding jackfruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,6 @@ streamlit.dataframe(my_data_row)
 
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thank you for adding ', fruit_choice)
+streamlit.write('Thank you for adding ', add_my_fruit)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
