67938b50b004539e33c1bfd3c245c1700e93c31f
Majid Mushtaq <120600148+Majid3556@users.noreply.github.com>
2023-04-16 21:11:25 +0530

    Update streamlit_app.py
    Added and imported some commands to control error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
+import pandas
 import streamlit
-
 import requests
-
 import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.title('My Parents New Healthy Diner')
@@ -15,7 +18,7 @@ streamlit.text('🥑🍞Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
