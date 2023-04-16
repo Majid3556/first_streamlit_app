6bd1fd57070b45572b8f465b361d024b368e70f7
Majid Mushtaq <120600148+Majid3556@users.noreply.github.com>
2023-04-16 20:52:46 +0530

    Update streamlit_app.py
    Added Lychee

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 # Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Lychee')
 streamlit.write('Thank you for adding ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
